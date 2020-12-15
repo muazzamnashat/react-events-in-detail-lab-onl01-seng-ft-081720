bc706c64103694d0a2c16aa12c2ca7d9a63a42fe
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-12-15 00:56:32 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,12 @@
 // Code CoordinatesButton Component Here
+import React from "react";
+
+export default class CoordinatesButton extends React.Component{
+    findCoordinates = (event)=>{
+        let coordinates = [event.clientX, event.clientY]
+        this.props.onReceiveCoordinates(coordinates)
+    }
+    render(){
+        return <button onClick={this.findCoordinates}></button>
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,14 @@
 // Code DelayedButton Component Here
+import React from "react";
+
+export default class DelayedButton extends React.Component{
+
+    delay = (event) =>{
+        event.persist();
+        window.setTimeout(this.props.onDelayedClick(event),this.props.delay);
+    }
+
+    render(){
+        return <button onClick={this.delay}></button>
+    }
+}
